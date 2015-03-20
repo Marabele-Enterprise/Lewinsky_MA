18d6b482abfe8e5b199844fab76e46b068c1b7ec
KwasiKK <kabelokwasi@gmail.com>
2015-03-20 13:29:04 +0200

    index controller updated

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -84,24 +84,6 @@ class Index extends Controller
     function messages()
     {
        $this->view->render('index/messages');
-    }
-
-    /**
-     * Handles what happens when user moves to URL/index/services, which is the same like URL/index or in this
-     * case even URL (without any controller/action) as this is the default controller-action when user gives no input.
-     */
-    function services()
-    {
-       $this->view->render('index/services');
-    }
-
-    /**
-     * Handles what happens when user moves to URL/index/portfolio, which is the same like URL/index or in this
-     * case even URL (without any controller/action) as this is the default controller-action when user gives no input.
-     */
-    function portfolio()
-    {
-       $this->view->render('index/portfolio');
-    }            
+    }   
     
 }
